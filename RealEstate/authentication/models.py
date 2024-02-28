804e224dbd488b3b97aa9d6479dc6bf0e17e87b8
StonecoldT <tausifislam2001@gmail.com>
2024-02-28 21:02:09 +0600

    models.py and forms.py implemented

--- RealEstate/authentication/models.py
+++ RealEstate/authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+
+    GENDER_CHOICES = [
+        (1, 'Male'),
+        (2, 'Female'),
+        (3, 'Other'),
+    ]
+    name = models.CharField(max_length = 50)
+    email = models.EmailField(max_length=100)
+    contact_no = models.CharField(max_length=20, default = None)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default = None)
+    nid = models.CharField(max_length=18, default=None)  # if Nid field is None, property dealing is cancelled.
+    dob = models.DateField(default = None)
+
+    address = models.TextField(default = None)
+    profile_picture = models.ImageField(upload_to='media/profile_picture', default = None)
+
+
+    
--- RealEstate/authentication/views.py
+++ RealEstate/authentication/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password']
+        password2 = request.POST['confirm_password']
+        
+
     return render(request, "signup.html")
 
 
