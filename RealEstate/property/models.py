ccee629a1b716a9a5f9c2001586cbbd181cc3aa6
StonecoldT <tausifislam2001@gmail.com>
2024-03-02 03:25:42 +0600

    views,models,urls of property

--- RealEstate/property/models.py
+++ RealEstate/property/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from authentication.models import UserProfile
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class AllProperty(models.Model):
     PROPERTY_TYPES = (
@@ -15,51 +15,73 @@ class AllProperty(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='properties', null = True)
     Property_Name = models.CharField(max_length=200)
     Property_Description = models.TextField(null=True, blank=True)
+    Total_area_in_sqft = models.DecimalField(max_digits=8, decimal_places=2,null=True)
     Price = models.IntegerField(default=0)
     Property_Pictures = models.ImageField(upload_to='pics',default=None)
     Road_No = models.CharField(max_length=4)
     Block = models.CharField(max_length=10)
-    city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=4)
+    City = models.CharField(max_length=100)
+    Postal_code = models.CharField(max_length=4)
     District = models.CharField(max_length=100)
     Property_on = models.CharField(max_length = 20, choices=Action, null =True)
-    property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES)
+    Property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES)
 
 
 
 class ResidentialProperty(AllProperty):
 
     House_No = models.CharField(max_length=8)
-    floor_count = models.PositiveIntegerField(default=1)
-    bedroom_count = models.PositiveIntegerField(default=1)
-    bathroom_count = models.PositiveIntegerField(default=1)
-    garage_spaces = models.PositiveIntegerField(default=0)
-    has_pool = models.BooleanField(default=False)
-    has_garden = models.BooleanField(default=False)
-    has_balcony = models.BooleanField(default=False)
-    year_built = models.DateField(null=True, blank=True)
+    Floor_count = models.PositiveIntegerField(default=1)
+    Bedrooms = models.PositiveIntegerField(default=1)
+    Bathrooms = models.PositiveIntegerField(default=1)
+    Garage_spaces_Per_Sqft = models.PositiveIntegerField(default=0)
+    Has_Pool = models.BooleanField(default=False)
+    Has_Garden = models.BooleanField(default=False)
+    Number_of_Balcony = models.PositiveIntegerField(default=1)
+    Year = models.IntegerField(
+        null=True,
+        blank=False,
+        validators=[
+        MinValueValidator(1900),
+        MaxValueValidator(2100)
+        ]
+    )
 
     
 
 
-
 class CommercialProperty(AllProperty):
-
+    Business_types = (
+        ('office', 'Office'),
+        ('community_Center', 'community_Center'),
+        ('shop', 'Shop'),
+        
+    )
     House_No = models.CharField(max_length=8)
-    business_type = models.CharField(max_length=100)
-    total_area = models.DecimalField(max_digits=8, decimal_places=2)
-    parking_spaces = models.PositiveIntegerField(default=0)
-    has_elevator = models.BooleanField(default=False)
-    has_security_system = models.BooleanField(default=False)
-    has_conference_room = models.BooleanField(default=False)
-    year_built = models.DateField(null=True, blank=True)
+    Business_type = models.CharField(max_length=20, choices=Business_types,null =True)
+    Parking_spaces = models.PositiveIntegerField(default=0)
+    Has_elevator = models.BooleanField(default=False)
+    Has_security_system = models.BooleanField(default=False)
+    Has_conference_room = models.BooleanField(default=False)
+    Year = models.IntegerField(
+        null=True,
+        blank=False,
+        validators=[
+        MinValueValidator(1900),
+        MaxValueValidator(2100)
+        ]
+    )
 
     
 
 class LandProperty(AllProperty):
-    land_area = models.DecimalField(max_digits=8, decimal_places=2)
-    land_type = models.CharField(max_length=100)
-    road_access = models.BooleanField(default=True)
-    is_fenced = models.BooleanField(default=False)
+    Land_types = (
+        ('Farmland','Farmland'),
+        ('Playground','Playground'),
+        ('warehouse','warehouse'),
+    )
+    Land_type = models.CharField(max_length=100, choices = Land_types,null = True)
+    Road_size_in_sqft = models.IntegerField
+    Is_fenced = models.BooleanField(default=False)
 
     
