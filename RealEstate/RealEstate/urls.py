10af10a39aca34194565dc28663484b260825752
BrainAxe007 <reshadromim013@gmail.com>
2024-03-02 00:41:35 +0600

    password reset function via mail

--- RealEstate/RealEstate/urls.py
+++ RealEstate/RealEstate/urls.py
@@ -18,11 +18,24 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
+from authentication import views as authentic_view
+from authentication.views import signin,password_reset_confirm 
 urlpatterns = [
     path('', include('basic.urls')),
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
     path('property/', include('property.urls')),
-    
+    path('login/', signin, name='login'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
+         name='password_reset'),
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', password_reset_confirm, name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
+    # Other URL patterns               
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- RealEstate/authentication/urls.py
+++ RealEstate/authentication/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('signup', views.signup, name = "signup"),
     path('signin', views.signin, name = "signin"),
     path('signout', views.signout, name = "signout"),
     path('profile', views.profile, name = "profile"),
     path('edit-profile', views.edit_profile, name = "edit_profile"),
+
 ]
