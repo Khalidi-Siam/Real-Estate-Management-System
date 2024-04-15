c49570a9195c7057fa658c13e7fb7dd8d8ebb587
BrainAxe007 <reshadromim013@gmail.com>
2024-04-15 20:14:54 +0600

    Payment auction done

--- RealEstate/Payment/views.py
+++ RealEstate/Payment/views.py
@@ -3,7 +3,6 @@ from django.views import View
 from django.urls import reverse
 import stripe
 from django.conf import settings
-from property.models import AllProperty
 from .models import *
 from authentication.models import UserProfile
 from django.shortcuts import redirect, render
@@ -11,18 +10,19 @@ from django.views import View
 from django.urls import reverse
 import stripe
 from django.conf import settings
-from property.models import AllProperty
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from datetime import datetime
-
+from auction.models import *
+from .models import *
+from authentication.models import *
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class CheckoutView(View):
     def get(self, request, property_id):
         try:
-            property = AllProperty.objects.get(id=property_id)
-        except AllProperty.DoesNotExist:
+            property = Auc_Property.objects.get(pk=property_id)
+        except Auc_Property.DoesNotExist:
             # Handle property not found
             return redirect('home')  # Redirect to home page or an error page
         
@@ -30,7 +30,7 @@ class CheckoutView(View):
 
     def post(self, request, *args, **kwargs):
         property_id = request.POST.get('property_id')
-        property = AllProperty.objects.get(id=property_id)
+        property = Auc_Property.objects.get(pk=property_id)
 
         # Create a checkout session with Stripe
         checkout_session = stripe.checkout.Session.create(
@@ -39,7 +39,7 @@ class CheckoutView(View):
                 {
                     'price_data': {
                         'currency': 'usd',  # Change to your currency
-                        'unit_amount': property.Price * 100,  # Convert price to cents
+                        'unit_amount': 2000 * 100,  # Convert price to cents
                         'product_data': {
                             'name': property.Property_Name,
                             # 'images': [property.Property_Pictures.url],  # Access the URL of the image
@@ -49,6 +49,9 @@ class CheckoutView(View):
                 },
             ],
             mode='payment',
+            metadata={
+                'property_id': property_id,
+            },
             success_url=request.build_absolute_uri(reverse('payment_success')+'?property_id=' + str(property.id)),
             cancel_url=request.build_absolute_uri(reverse('payment_cancel')+'?property_id=' + str(property.id)),
         )
@@ -59,13 +62,23 @@ def PaymentSuccessView(request):
 
     # Get the property and user details based on the request
     property_id = request.GET.get('property_id')  # Example: Property ID passed as a URL parameter
-    user_id = request.user.id  # Assuming you're using Django's built-in authentication
+    user_id = request.user.UserProfile
+    user_name =  request.user.UserProfile.name
     
     # Query the database to get the property details
     try:
-        property = AllProperty.objects.get(id=property_id)
+        property = Auc_Property.objects.get(id=property_id)
         property_name = property.Property_Name
-    except AllProperty.DoesNotExist:
+        payment = PaymentDetails.objects.create(name = user_name,
+                                                property_name = property_name,
+                                                payment_time= datetime.now,
+                                                payment_amount=2000.00,
+                                                property_id = property,
+                                                owner_id = user_id
+                                                )
+        payment.save()
+
+    except Auc_Property.DoesNotExist:
         property_name = "Unknown Property"
 
     # Get the user's name
@@ -83,9 +96,9 @@ def PaymentCancelView(request):
     user_id = request.user.id  # Assuming you're using Django's built-in authentication
     
     try:
-        property = AllProperty.objects.get(id=property_id)
+        property = Auc_Property.objects.get(id=property_id)
         property_name = property.Property_Name
-    except AllProperty.DoesNotExist:
+    except Auc_Property.DoesNotExist:
         property_name = "Unknown Property"
 
     buyer_name = request.user.UserProfile.name
@@ -119,16 +132,12 @@ def my_webhook_view(request):
      print('ok')
 
      if session.payment_status == 'paid':
-        line_item = session.list.line_items(session.id, limit=1).data[0]
-        order_id = line_item['description']
-        fulfill_order(order_id)
+            order_id = session.metadata.get('property_id')
+            fulfill_order(order_id)
 
   return HttpResponse(status=200)
 
 def fulfill_order(order_id):
-   print('ok')
-   print(order_id)
-   order = AllProperty.objects.get(id=order_id)
-   order.sold = True
-   order.soldDate = datetime.now()
-   order.save()
+    order = Auc_Property.objects.get(id=order_id)
+    order.Property_Status = 'Sold'
+    order.save()
--- RealEstate/auction/forms.py
+++ RealEstate/auction/forms.py
@@ -8,22 +8,38 @@ class Auction_PropertyForm(forms.ModelForm):
         model = Auc_Property
         fields = '__all__'
         exclude = ('seller','winner','end_time','current_price')
+        
 
 class Auction_ResidentialPropertyForm(forms.ModelForm):
     class Meta:
         model = Auc_ResidentialProperty
         exclude = ('Property_type','seller','winner','end_time','current_price')  # Excluding Property_type because it's already defined in the parent class
-
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Year'].widget = forms.DateInput(attrs={'type': 'date'})
+        self.fields['Block'].required = False
+        self.fields['Block'].label = "Block/Sector"
+        
 class Auction_CommercialPropertyForm(forms.ModelForm):
     class Meta:
         model = Auc_CommercialProperty
         exclude = ('Property_type','seller','winner','end_time','current_price') # Excluding Property_type because it's already defined in the parent class
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Year'].widget = forms.DateInput(attrs={'type': 'date'})
+        self.fields['Block'].required = False
+        self.fields['Block'].label = "Block/Sector"
 
 class Auction_LandPropertyForm(forms.ModelForm):
     class Meta:
         model = Auc_LandProperty
         exclude = ('Property_type','seller','winner','end_time','current_price')  # Excluding Property_type because it's already defined in the parent class
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Year'].widget = forms.DateInput(attrs={'type': 'date'})
+        self.fields['Block'].required = False
+        self.fields['Block'].label = "Block/Sector"
 
 class BidForm(forms.ModelForm):
     class Meta:
